04520b3a5776b369c11c52164fb76e9d17678cf7
marinav2211 <marinochka2288@gmail.com>
2024-02-19 19:17:22 +0100

    fix

--- app/core/tests/test_command.py
+++ app/core/tests/test_command.py
@@ -1,9 +1,9 @@
 '''
-Test custom Django management command
+Test custom Django management commands
 '''
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
