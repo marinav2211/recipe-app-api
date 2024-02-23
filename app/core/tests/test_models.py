f11ebd5260c6f4e39c1e620de46d26556980f746
marinav2211 <marinochka2288@gmail.com>
2024-02-23 03:19:32 +0100

    fix

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -11,12 +11,12 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
 
-    email = 'test@example.com'
-    password = 'testpass123'
-    user = get_user_model().objects.create_user(
-        email=email,
-        password=password,
-    )
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
 
-    self.assertEqual(user.email, email)
-    self.assertEqual(user.check_password(password))
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
